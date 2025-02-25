2f2298b501541e5ed835766338427e37d93004c2
mehulmoon <117886098+mehulmoon@users.noreply.github.com>
2025-02-25 18:20:05 +0530

    Updates

---
+++ precache-manifest.ad5af0df5bc7f14718276c4c7ada0c14.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9a8d7e239a2ce2d0d8d30d6884ff9c",
+    "url": "/port-folio/index.html"
+  },
+  {
+    "revision": "68363e19c424191e5f4c",
+    "url": "/port-folio/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "72858469a0e0f0cfdd06",
+    "url": "/port-folio/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "68363e19c424191e5f4c",
+    "url": "/port-folio/static/js/2.098acdc1.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/port-folio/static/js/2.098acdc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72858469a0e0f0cfdd06",
+    "url": "/port-folio/static/js/main.752c0940.chunk.js"
+  },
+  {
+    "revision": "3e6e679ee8f5f6a54be4",
+    "url": "/port-folio/static/js/runtime-main.4afd3a91.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/port-folio/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/port-folio/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/port-folio/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/port-folio/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/port-folio/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/port-folio/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/port-folio/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/port-folio/static/media/software.43049cae.jpeg"
+  }
+]);
